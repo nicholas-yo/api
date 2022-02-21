3e639a6c50db836210a2496b8f6b400d932686d6
Nícolas <83851966+nicholas-yo@users.noreply.github.com>
2022-02-21 09:29:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,32 @@ import { cyanBright } from 'cli-color';
 import process from 'process';
 
 (async () => {
+  const { createWriteStream } = await import('fs');
+  const { Console } = await import('console');
+  const { join } = await import('path');
+
   const { default: server } = await import('./server/index');
   const {
     default: { hostname, port, protocol }
   } = await import('./config/url');
 
+  const output = createWriteStream(join(__dirname, '../src/logs/stdout.log'));
+  const errorOutput = createWriteStream(
+    join(__dirname, '../src/logs/stderr.log')
+  );
+
+  const logger = new Console({
+    stdout: output,
+    stderr: errorOutput
+  });
+
+  const msg = `[${process.pid}] - 🚀 Running at ${protocol}${hostname}:${port}`;
+
+  logger.log(msg);
+
   server.listen(port, () => {
     const init = (msg: string) => cyanBright(msg);
 
-    console.log(
-      init(`[${process.pid}] - 🚀 Running at ${protocol}${hostname}:${port}`)
-    );
+    process.stdout.write(init(`${msg}\n`));
   });
-
-  process.on('exit', () => server.close());
 })();
